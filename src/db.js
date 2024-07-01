a5559b5b21669fbe40114dde0acf3f3ae1c8f75e
neko-zilla <109001868+neko-zilla@users.noreply.github.com>
2024-06-30 21:45:59 -0500

    Update db.js

--- src/db.js
+++ src/db.js
@@ -1,10 +1,10 @@
 import mysql from 'mysql2/promise'; 
 // Configuración de la conexión a la base de datos
 export const pool = mysql.createPool({
-  host: 'localhost',     // Cambia según la configuración de tu base de datos
+  host: 'monorail.proxy.rlwy.net',     // Cambia según la configuración de tu base de datos
   user: 'root',          // Cambia según tu nombre de usuario de la base de datos
-  password: '',          // Cambia según tu contraseña de la base de datos
-  database: 'cartas',    // Cambia según el nombre de tu base de datos
+  password: 'tqMyXRdNQBWGErdUyteeJAgGxaPGLWQr',          // Cambia según tu contraseña de la base de datos
+  database: 'railway',    // Cambia según el nombre de tu base de datos
   waitForConnections: true,
   connectionLimit: 10,
   queueLimit: 0
